af6e76764616634d7571ae72255d1c1ec2a32514
Rob Meijer <pibara@gmail.com>
2022-04-19 13:14:00 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -6,14 +6,21 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='physicalquantity',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple library for working with physical quantities',
     long_description="""A simple library for working with physical quantities. 
     Implements basic dimensional decomposition of physical quantities and provides
     basic operations (adition, subtraction, multiplication, division, comparison) 
     for working with these quantities.
 
-    Support for non-SI units is currently partialy supported, better support in under way. 
+    Support for non-SI units is available but most operations will result in 
+    implicit conversions to SI units. Use the as_absolute() or as_relative()
+    methods to convert back to the desired non-SI units.
+
+    Note that while this library supports a wide range of the dimentional analysis
+    and related integrity artifacts of working with physical quantities, the prime 
+    goal of this library isn't the dimentional integrity of code, but instead the
+    unified serialization or rather serializisability of physical quantities. 
 
     """,
     url='https://github.com/pibara/aioflureedb',
